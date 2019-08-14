518f205f52ea945d7278e8ca68bf5a965671ffce
victor <victooralmeida1@gmail.com>
2019-08-14 10:22:53 -0300

    acervo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
