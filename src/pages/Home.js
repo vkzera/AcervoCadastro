60e1165b248262e8cd5ae238fb943e2880af50b0
Victor Almeida <victooralmeida1@gmail.com>
2019-08-14 10:37:55 -0300

    atualização dados

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,62 +1,61 @@
-import React, { Component } from 'react';
-import api from '../services/api';
-import './styles.css';
+import React, { Component } from "react";
+import api from "../services/api";
+import "./styles.css";
 
 class Home extends Component {
+  state = {
+    nomeLivro: "",
+    autorLivro: "",
+    sinopseLivro: ""
+  };
 
-    state = {
-        nomeLivro: '',
-        autorLivro: '',
-        sinopseLivro: '',
-    };
+  handleSubmit = async e => {
+    e.preventDefault();
 
-    handleSubmit = async e => {
-        e.preventDefault();
+    const data = new FormData();
+    data.append("nomeLivro", this.state.nomeLivro);
+    data.append("autorLivro", this.state.autorLivro);
+    data.append("sinopseLivro", this.state.sinopseLivro);
+    await api.post("livros", data);
+    alert("Cadastro enviado.");
+    this.setState({ nomeLivro: "", autorLivro: "", sinopseLivro: "" });
+  };
 
-        const data = new FormData();
-        data.append('nomeLivro', this.state.nomeLivro);
-        data.append('autorLivro', this.state.autorLivro);
-        data.append('sinopseLivro', this.state.sinopseLivro);
-        await api.post('livros', data);
-        alert("Cadastro enviado.");
-    }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
+  render() {
+    return (
+      <div>
+        <form action="" id="new-post" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="nomeLivro"
+            placeholder="Nome do livro"
+            onChange={this.handleChange}
+            value={this.state.nomeLivro}
+          />
+          <input
+            type="text"
+            name="autorLivro"
+            placeholder="Autor do livro"
+            onChange={this.handleChange}
+            value={this.state.autorLivro}
+          />
+          <textarea
+            type="text"
+            name="sinopseLivro"
+            placeholder="Sinopse do livro"
+            onChange={this.handleChange}
+            value={this.state.sinopseLivro}
+          />
 
-
-    render() {
-        return (
-            <div>
-                <form action="" id="new-post" onSubmit={this.handleSubmit}>
-                    <input
-                        type='text'
-                        name='nomeLivro'
-                        placeholder='Nome do livro'
-                        onChange={this.handleChange}
-                        value={this.state.nomeLivro}
-                    />
-                    <input
-                        type='text'
-                        name='autorLivro'
-                        placeholder='Autor do livro'
-                        onChange={this.handleChange}
-                        value={this.state.autorLivro}
-                    />
-                    <textarea
-                        type='text'
-                        name='sinopseLivro'
-                        placeholder='Sinopse do livro'
-                        onChange={this.handleChange}
-                        value={this.state.sinopseLivro}
-                    />
-
-                    <button type="submit">Enviar</button>
-                </form>
-            </div>
-        );
-    }
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://help-canta-sys-backend.herokuapp.com/api'
+  baseURL: process.env.BASE_URL
 });
 
 export default api;
